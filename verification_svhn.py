53f567fe2e5cbeb4928d04277e879bfc1011386f
Rémi BERNHARD <44685205+RemiBERNHARD@users.noreply.github.com>
2020-02-03 08:26:19 +0100

    Update verification_svhn.py

--- verification_svhn.py
+++ verification_svhn.py
@@ -14,8 +14,6 @@ from keras.preprocessing.image import ImageDataGenerator
 from keras import backend 
 import tensorflow as tf
 import numpy as np
-import random
-random.seed(123)
 from datetime import datetime
 
 from utils_func import metrics, clip_adv, clip_adv_l2
